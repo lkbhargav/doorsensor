313c9ba53a512348bb194c0057a2e34d706b1b20
lkbhargav <lkbhargav9@gmail.com>
2023-03-26 13:18:50 -0400

    Blinking LED change

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use rppal::gpio::Gpio;
+use rppal::system::DeviceInfo;
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+
+// Gpio uses BCM pin numbering. BCM GPIO 18 is tied to physical pin 12.
+const GPIO_LED: u8 = 18;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+
+    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
+
+    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_input();
+
+    // Blink the LED by setting the pin's logic level high for 500 ms.
+    pin.set_high();
+    thread::sleep(Duration::from_millis(500));
+    pin.set_low();
+
+    Ok(())
+}
